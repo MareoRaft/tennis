3e6d734bdbdafbb8b6e7b47492560b321fe081b7
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-10-13 09:13:10 -0400

    turn on auto-reload on dev server (debug=True)

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ def index():
 # gunicorn doesn't run this.  it grabs the app var and runs/hosts it itself
 if __name__ == '__main__':
 	# port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000, debug=True)
